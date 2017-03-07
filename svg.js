84e11177cd8712f564e1a89a18b5848a395f42ab
Fortune Soleil <fsoleil@stuy.edu>
2017-03-07 13:30:41 -0500

    Add files via upload

---
+++ svg.js
@@ -0,0 +1,79 @@
+var pic = document.getElementById("vimage");
+var clr = document.getElementById("clear");
+var xpn = document.getElementById("grow");
+
+var lastX = -1;
+var lastY;
+
+var shape = function(event) {
+  var c = document.createElementNS("http://www.w3.org/2000/svg",
+ "circle");
+  c.setAttribute( "cx", event.clientX );
+  c.setAttribute( "cy", event.clientY );
+  c.setAttribute( "r", "50" );
+  c.setAttribute( "fill", "red");
+  c.setAttribute( "stroke", "black" );
+  pic.appendChild( c );
+  line(event);
+};
+
+var line = function(event) {
+  if (lastX < 0){
+    lastX = event.clientX;
+    lastY = event.clientY;
+  }
+  else{
+    drawline(lastX, lastY, event);
+    lastX = event.clientX;
+    lastY = event.clientY;
+  }
+};
+
+var drawline = function( x, y, event ) {
+  var c = document.createElementNS("http://www.w3.org/2000/svg",
+ "line");
+  c.setAttribute( "x1", x );
+  c.setAttribute( "y1", y );
+  c.setAttribute( "x2", event.clientX );
+  c.setAttribute( "y2", event.clientY );
+  c.setAttribute( "stroke", "black" );
+  pic.appendChild( c );
+};
+
+var clear = function() {
+  while(pic.childNodes.length > 0)
+    pic.removeChild(pic.childNodes[0]);
+  lastX = -1;
+};
+
+var resize = function(){
+  var rad = 50;
+  var growth = true;
+  var thing = function(){
+    while (pic.childNodes.length > 0)
+      pic.removeChild(pic.childNodes[0]);
+    var c = document.createElementNS("http://www.w3.org/2000/svg",
+   "circle");
+    c.setAttribute( "cx", 250 );
+    c.setAttribute( "cy", 250 );
+    c.setAttribute( "r", rad );
+    c.setAttribute( "fill", "red");
+    c.setAttribute( "stroke", "black" );
+    pic.appendChild( c );
+    if (rad == 250) growth = false;
+    else if (rad == 50) growth = true;
+    if (growth) rad++;
+    else rad--;
+  };
+  var intervalID = window.setInterval( thing, 16 );
+};
+
+clr.addEventListener("mousedown", clear);
+pic.addEventListener("mousedown", shape);
+xpn.addEventListener("mousedown", resize);
+
+//html tags
+//<circle cx="250" cy="250" r="75" fill="yellow"
+//stroke="black"/>
+//   <rect x="100" y="100" width="300" height="75"
+//fill="blue"/>
